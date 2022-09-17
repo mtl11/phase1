791ec77fbea8fa7437b52c6f9f3e93f940d15d7a
mtl11 <62129086+mtl11@users.noreply.github.com>
2022-09-16 23:49:05 -0700

    Initial commit
    added dummy functions

---
+++ phase1.c
@@ -0,0 +1,44 @@
+#include <phase1.h>
+
+void phase1_init(void){
+
+}
+int   fork1(char *name, int(*func)(char *), char *arg, int stacksize, int priority){
+	return 0;
+}
+
+int   join(int *status){
+	return 0;
+}
+void  quit(int status){
+}
+ int   zap(int pid){
+	return 0;
+}
+ int   isZapped(void){
+return 0;
+} 
+int   getpid(void){
+return 0;
+}
+ void  dumpProcesses(void){
+}
+ int   blockMe(int block_status){
+return 0;
+}
+ int   unblockProc(int pid){
+return 0;
+}
+ int   readCurStartTime(void){
+return 0;
+}
+ void  timeSlice(void){
+}
+ int   readtime(void){
+return 0;
+} 
+
+// never returns!
+void  startProcesses(void){
+}   
+
